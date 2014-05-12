b69532f71fa83c3e4d0c5fe8e541a535a9cc5b09
vfpeixoto <vfernandespeixoto@gmail.com>
2014-05-12 14:19:38 -0300

    Busca do fluxo de caixa

--- fluxocaixa/urlsFluxo.py
+++ fluxocaixa/urlsFluxo.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('fluxocaixa.views',
     url(r'^$', 'fluxoListar'),
     url(r'^pesquisar/$', 'fluxoPesquisar'),
+    url(r'^imprimir/$', 'fluxoImprimir'),
 )
--- fluxocaixa/views.py
+++ fluxocaixa/views.py
@@ -13,8 +13,8 @@ def fluxoListar(request):
 def fluxoPesquisar(request):
     if request.method == 'POST':
         pessoaBusca = request.POST.get('pessoaBusca')
-        #dataBuscaInicio = datetime.strptime(request.POST.get('dataBuscaInicio', ''), '%d/%m/%Y %H:%M:%S')
-        #dataBuscaFim = datetime.strptime(request.POST.get('dataBuscaFim', ''), '%d/%m/%Y %H:%M:%S')
+        dataBuscaInicio = datetime.strptime(request.POST.get('dataBuscaInicio', ''), '%d/%m/%Y %H:%M:%S')
+        dataBuscaFim = datetime.strptime(request.POST.get('dataBuscaFinal', ''), '%d/%m/%Y %H:%M:%S')
         
         nome = Pessoa.objects.filter(id=pessoaBusca)
         pessoas = Pessoa.objects.all().order_by('nome')
@@ -23,8 +23,8 @@ def fluxoPesquisar(request):
         totalpagar = 0
        
         try:
-            contas = Conta.objects.filter(
-                    (Q(pessoa_id=pessoaBusca))).order_by('-data')   
+            sql = "select * from caixas_conta where pessoa_id like %s  and data >= '%s' and  data <= '%s'" % (pessoaBusca, dataBuscaInicio, dataBuscaFim)
+            contas = Conta.objects.raw(sql)
 
             for item in contas:
                 if item.tipo == 'E':
@@ -36,3 +36,5 @@ def fluxoPesquisar(request):
             contas = [Conta(descricao='erro')]
 
         return render(request, 'fluxos/fluxoListar.html', {'contas': contas, 'nome':nome, 'pessoas': pessoas, 'totalreceber':totalreceber,'totalpagar':totalpagar})
+
+
