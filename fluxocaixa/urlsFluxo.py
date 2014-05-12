a959f4fd06eece5a23790815c312b43d80385ad9
vfpeixoto <vfernandespeixoto@gmail.com>
2014-05-12 16:43:05 -0300

    Refatoração 1

--- fluxocaixa/urlsFluxo.py
+++ fluxocaixa/urlsFluxo.py
@@ -3,5 +3,4 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('fluxocaixa.views',
     url(r'^$', 'fluxoListar'),
     url(r'^pesquisar/$', 'fluxoPesquisar'),
-    url(r'^imprimir/$', 'fluxoImprimir'),
 )
--- fluxocaixa/views.py
+++ fluxocaixa/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render, HttpResponseRedirect
 from datetime import datetime
-from django.db.models import Q 
 from caixas.models import Conta
 from pessoas.models import Pessoa
 
 def fluxoListar(request):
-    fluxo = Conta
     pessoas = Pessoa.objects.all().order_by('nome')
-
-    return render(request, 'fluxos/fluxoListar.html', {'fluxo': fluxo, 'pessoas': pessoas})
+    return render(request, 'fluxos/fluxoListar.html', {'pessoas': pessoas})
 
 def fluxoPesquisar(request):
     if request.method == 'POST':
